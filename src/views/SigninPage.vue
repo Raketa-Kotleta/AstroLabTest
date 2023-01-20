6033b5516098ead8b8bec42690684543f9af3aaa
Raketa-Kotleta <niceikis@gmail.com>
2023-01-20 12:34:35 +0300

    start configuration

---
+++ src/views/SigninPage.vue
@@ -0,0 +1,15 @@
+<template>
+<form action="">
+
+</form>
+</template>
+
+<script>
+export default{
+    name: "SigninPage",
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/SignupPage.vue
@@ -0,0 +1,15 @@
+<template>
+<form action="">
+
+</form>
+</template>
+
+<script>
+export default{
+    name: "SignupPage",
+}
+</script>
+
+<style lang="scss">
+
+</style>
